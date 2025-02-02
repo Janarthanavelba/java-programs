75b4a7295ea418ba1c8964c23573572d3e86c6ef
Janarthanavel K <129484927+Janarthanavelba@users.noreply.github.com>
2025-02-02 11:01:29 +0530

    Create Happy_numbers.java

---
+++ Happy_numbers.java
@@ -0,0 +1,31 @@
+/*Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.*/
+
+class Solution {
+    public boolean isHappy(int n) {
+        int m=n;
+        while(m != 1 && m != 4)
+        {
+        n=m;
+        int sum=0;
+        while(n>0)
+        {
+            int a=n%10;
+            int sq=a*a;
+            sum=sum+sq;
+            n/=10;
+            
+        }
+        m=sum;
+        }
+        if(m==1) return true;
+        else return false;
+        
+    }
+}
